cba0931c95ed8070c55e64f0ac31edcee9df05e7
andyhsiungdev <andy.hsiung.dev@gmail.com>
2026-07-14 10:49:44 -0700

    nvim: add which-key and telescope plugins

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+-- Fuzzy finder over files, buffers, grep, and more. plenary.nvim is a required
+-- Lua utility dependency. Loads lazily on its keymaps.
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
+      { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
+    },
+    opts = {},
+  },
+}
---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+-- Popup that shows available keybindings as you type a prefix (e.g. <leader>).
+-- Loads on VeryLazy so it never delays startup.
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      preset = "modern",
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+}
